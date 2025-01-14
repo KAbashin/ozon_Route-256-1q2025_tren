af71f582057442b53b0a0c9c69586b371b001e67
Konstantin Abashin <99419296+KAbashin@users.noreply.github.com>
2025-01-14 14:42:25 +0300

    Create main.go

---
+++ 4 Планировщик заказов/main.go
@@ -0,0 +1,12 @@
+/*
+ Рекомендуется использовать быстрый (буферизованный) ввод и вывод
+var in *bufio.Reader
+var out *bufio.Writer
+in = bufio.NewReader(os.Stdin)
+out = bufio.NewWriter(os.Stdout)
+defer out.Flush()
+
+var a, b int
+fmt.Fscan(in, &a, &b)
+fmt.Fprint(out, a + b)
+*/
