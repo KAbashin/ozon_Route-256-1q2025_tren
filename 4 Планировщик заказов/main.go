8ab9244e6ed3d24baaec881dedca9aadc739fdea
kabashin <kabashin@yandex.ru>
2025-01-15 17:59:21 +0300

    Задача 4 в процессе

--- 4 Планировщик заказов/main.go
+++ 4 Планировщик заказов/main.go
@@ -1,12 +1,83 @@
-/*
- Рекомендуется использовать быстрый (буферизованный) ввод и вывод
-var in *bufio.Reader
-var out *bufio.Writer
-in = bufio.NewReader(os.Stdin)
-out = bufio.NewWriter(os.Stdout)
-defer out.Flush()
+package main
 
-var a, b int
-fmt.Fscan(in, &a, &b)
-fmt.Fprint(out, a + b)
-*/
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Order struct {
+	index    int
+	incoming int // время поступления заказа
+}
+
+type Machine struct {
+	start    int // время начала
+	end      int // время окончания
+	capacity int // мах свободное место
+	index    int
+	// currentFreeSpace int // оставшееся свободное место
+}
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	// принимаем количество наборов
+	var n int
+	fmt.Fscanln(in, &n) // количество наборов
+
+	for i := 0; i < n; i++ {
+		// прием наборов
+		var quantityOffers int
+
+		fmt.Fscanln(in, &quantityOffers) // количество офферов
+
+		arrivalOffers, _ := in.ReadString('\n') // поступление офферов
+		elementsOffers := strings.Fields(arrivalOffers)
+		arrivalStruct := make([]Order, 0)
+		for i, s := range elementsOffers {
+			incoming, _ := strconv.Atoi(s)
+			arrivalStruct = append(arrivalStruct, Order{index: i + 1, incoming: incoming})
+		}
+
+		var mashinsCount int
+		trucksStruckt := make([]Machine, mashinsCount)
+		fmt.Fscanln(in, &mashinsCount) // количество машин
+
+		for j := 0; j < mashinsCount; j++ {
+			var start, end, capacity int
+			fmt.Fscan(in, &start, &end, &capacity) // параметры машины
+			trucksStruckt = append(trucksStruckt, Machine{start: start, end: end, capacity: capacity, index: j + 1})
+		}
+
+		sort.Slice(arrivalStruct, func(i, j int) bool {
+			return arrivalStruct[i].incoming < arrivalStruct[j].incoming
+		})
+		sort.Slice(trucksStruckt, func(i, j int) bool {
+			return trucksStruckt[i].start < trucksStruckt[j].start
+		})
+
+		// берем в цикле заказы
+		// ищем машину со стартом >= закзау
+		// проверяем эенд <= заказу
+		// проверяем capacity >= 1
+		// capacity уменьшаем на 1
+
+		fmt.Fprintln(out, arrivalStruct) // вывод для отладки
+		fmt.Fprintln(out, trucksStruckt) // вывод для отладки
+
+		result := make([]int, quantityOffers)
+		strSlice := make([]string, len(result))
+		for i, v := range result {
+			strSlice[i] = fmt.Sprintf("%d", v)
+		}
+		fmt.Fprintln(out, strings.Join(strSlice, " "))
+
+	}
+
+}
